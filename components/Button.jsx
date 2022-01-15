62ef6534f1747460dc46cf36c64820b16e7fe7df
luvagu <luiavag@gmail.com>
2022-01-15 16:43:49 -0500

    removed flex

--- components/Button.jsx
+++ components/Button.jsx
@@ -5,7 +5,7 @@ function Button({ type = 'button', variant = 'full', onClick, children }) {
 		<button
 			type={type}
 			className={classNames(
-				'inline-flex justify-center py-2 px-4 border shadow-sm text-sm font-medium rounded-md hover:bg-blue-700 focus:ring-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2',
+				'py-2 px-4 border shadow-sm text-sm font-semibold rounded-md hover:bg-blue-700 focus:ring-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2',
 				variant === 'outlined'
 					? 'border-blue-600 text-blue-600 hover:text-white'
 					: 'border-transparent text-white bg-blue-600'
