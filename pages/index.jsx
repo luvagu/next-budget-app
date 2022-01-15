3a4b8d410be2c067c38e618bdd32dd8464b2eb19
luvagu <luiavag@gmail.com>
2022-01-15 17:42:04 -0500

    added CardsGrid, fixed imports

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
-import Container from '../components/Container'
-import Header from '../components/Header'
+import Container from '../components/shared/Container'
 import CardsGrid from '../components/CardsGrid'
+import Header from '../components/Header'
 
 export default function Home() {
 	return (
