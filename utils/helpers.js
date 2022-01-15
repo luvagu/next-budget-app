85b1ac2139cda297b57bbb7dd477eb13abfe5c8e
luvagu <luiavag@gmail.com>
2022-01-15 17:51:58 -0500

    added curencyFormatter

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,11 @@
 export function classNames(...classes) {
 	return classes.filter(Boolean).join(' ')
 }
+
+export function curencyFormatter(amount) {
+	return new Intl.NumberFormat(undefined, {
+		currency: 'usd',
+		style: 'currency',
+		minimumFractionDigits: 0,
+	}).format(amount)
+}
