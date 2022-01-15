44c6d6b5e13b51a834bf25bc7968feaa5bbb4093
luvagu <luiavag@gmail.com>
2022-01-15 16:02:57 -0500

    added Container and Stack utility components

---
+++ components/Stack.jsx
@@ -0,0 +1,19 @@
+import { classNames } from '../utils/twcss.helpers'
+
+function Stack({ children, direction = 'vertical', extraClass }) {
+	return (
+		<div
+			className={classNames(
+				'flex self-stretch',
+				direction === 'vertical'
+					? 'flex-auto flex-col '
+					: 'flex-row items-center',
+				...extraClass
+			)}
+		>
+			{children}
+		</div>
+	)
+}
+
+export default Stack
