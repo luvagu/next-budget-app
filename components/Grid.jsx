8a32569853fcc9522af474c6e76acb920a0f8452
luvagu <luiavag@gmail.com>
2022-01-15 17:01:04 -0500

    added Grid util comp

---
+++ components/Grid.jsx
@@ -0,0 +1,9 @@
+function Grid({ children }) {
+	return (
+		<div className='grid grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))] gap-4 items-start'>
+			{children}
+		</div>
+	)
+}
+
+export default Grid
