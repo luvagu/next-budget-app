2b3758c44026f8ce08806c44a98f2754c89bc074
luvagu <luiavag@gmail.com>
2022-01-15 16:03:28 -0500

    renamed files with .jsx

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import Container from '../components/Container'
+import Header from '../components/Header'
+
+export default function Home() {
+	return (
+		<Container>
+			<Header />
+		</Container>
+	)
+}
