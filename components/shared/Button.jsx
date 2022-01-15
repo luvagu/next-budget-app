9af45ba8a7e0fd124aea9e5ce83d99dce808be81
luvagu <luiavag@gmail.com>
2022-01-15 17:23:31 -0500

    moved to shared folder

--- components/shared/Button.jsx
+++ components/shared/Button.jsx
@@ -1,11 +1,11 @@
-import { classNames } from '../utils/twcss.helpers'
+import { classNames } from '../../utils/twcss.helpers'
 
 function Button({ type = 'button', variant = 'full', onClick, children }) {
 	return (
 		<button
 			type={type}
 			className={classNames(
-				'py-2 px-4 border shadow-sm text-sm font-semibold rounded-md hover:bg-blue-700 focus:ring-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2',
+				'py-2 px-4 border shadow-sm text-sm font-semibold whitespace-nowrap rounded-md hover:bg-blue-700 focus:ring-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2',
 				variant === 'outlined'
 					? 'border-blue-600 text-blue-600 hover:text-white'
 					: 'border-transparent text-white bg-blue-600'
