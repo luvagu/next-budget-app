bd35642153964bd2b396cb34f7d96d448003108c
luvagu <luiavag@gmail.com>
2022-01-15 19:22:16 -0500

    added ProgressBar with color variants

---
+++ components/shared/ProgressBar.jsx
@@ -0,0 +1,31 @@
+import { calculatePercent, classNames } from '../../utils/helpers'
+
+const variants = {
+	blue: 'bg-blue-500',
+	yellow: 'bg-yellow-500',
+	red: 'bg-red-500',
+}
+
+function ProgressBar({ current, min = 0, max }) {
+	const getVariant = () => {
+		const percent = calculatePercent(current, max)
+		if (percent < 50) return variants.blue
+		if (percent < 75) return variants.yellow
+		return variants.red
+	}
+
+	return (
+		<div className='h-4 bg-gray-300 rounded-lg overflow-hidden transition'>
+			<div
+				className={classNames(
+					'h-4',
+					getVariant(),
+					'transition-[width] duration-300'
+				)}
+				style={{ width: `${calculatePercent(current, max)}%` }}
+			></div>
+		</div>
+	)
+}
+
+export default ProgressBar
