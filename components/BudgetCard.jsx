39334f4416d901c295d46f5d9f1f320c16cc29ce
luvagu <luiavag@gmail.com>
2022-01-15 19:47:30 -0500

    contructed BudgetCard

--- components/BudgetCard.jsx
+++ components/BudgetCard.jsx
@@ -1,11 +1,19 @@
 import { curencyFormatter } from '../utils/helpers'
+import Button from './shared/Button'
 import Card from './shared/Card'
 import ProgressBar from './shared/ProgressBar'
 import Stack from './shared/Stack'
 
-function BudgetCard({ name, amount, max }) {
+function BudgetCard({ name, amount, max, gray }) {
+	let background = ''
+	if (amount > max) {
+		background = 'bg-red-100'
+	} else if (gray) {
+		background = 'bg-gray-100'
+	}
+
 	return (
-		<Card>
+		<Card extraClass={background}>
 			<h2 className='flex justify-between items-baseline text-gray-500 font-semibold text-xl whitespace-nowrap mb-3'>
 				<div className='mr-2'>{name}</div>
 				<div className='flex items-baseline'>
@@ -16,7 +24,12 @@ function BudgetCard({ name, amount, max }) {
 				</div>
 			</h2>
 			<ProgressBar current={amount} max={max} />
-			<Stack direction='horizontal'></Stack>
+			<Stack direction='horizontal' extraClass='gap-2 mt-4'>
+				<Button extraClass='ml-auto'>Add Expense</Button>
+				<Button variant='gray-outline' color='gray'>
+					View Expenses
+				</Button>
+			</Stack>
 		</Card>
 	)
 }
