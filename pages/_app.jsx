37fd8b9567aa8f488cedec0c225b9f849db791fe
luvagu <luiavag@gmail.com>
2022-01-15 20:21:20 -0500

    wrapped app in BudgetsProvider

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,13 @@
+import BudgetsProvider from '../context/BudgetsContext'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return (
+		<BudgetsProvider>
+			<Component {...pageProps} />
+		</BudgetsProvider>
+	)
 }
 
 export default MyApp
