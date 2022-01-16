3c2829bde439008b4d61d7735ea050d889799a67
luvagu <luiavag@gmail.com>
2022-01-16 03:26:21 -0500

    added ViewExpenses modal and triggers

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,29 +4,39 @@ import AddBudget from './AddBudget'
 import AddExpense from './AddExpense'
 import Button from './shared/Button'
 import Stack from './shared/Stack'
+import ViewExpenses from './ViewExpenses'
 
 function Header() {
 	const {
 		openAddBudgetModal,
-		toggleBudgetModal,
+		toggleAddBudgetModal,
 		openAddExpenseModal,
-		toggleExpenseModal,
-		openExpenseModalWithId,
+		toggleAddExpenseModal,
+		openAddExpenseModalWithId,
+		openViewExpenseModal,
+		toggleViewExpenseModal,
 	} = useBadgets()
 
 	return (
 		<Fragment>
 			<Stack direction='horizontal' extraClass='gap-2 mb-4'>
 				<h1 className='text-3xl font-semibold mr-auto'>Budgets</h1>
-				<Button onClick={toggleBudgetModal}>Add Budget</Button>
-				<Button onClick={openExpenseModalWithId} variant='blue-outline'>
+				<Button onClick={toggleAddBudgetModal}>Add Budget</Button>
+				<Button onClick={openAddExpenseModalWithId} variant='blue-outline'>
 					Add Expense
 				</Button>
 			</Stack>
-			<AddBudget isOpen={openAddBudgetModal} closeModal={toggleBudgetModal} />
+			<AddBudget
+				isOpen={openAddBudgetModal}
+				closeModal={toggleAddBudgetModal}
+			/>
 			<AddExpense
 				isOpen={openAddExpenseModal}
-				closeModal={toggleExpenseModal}
+				closeModal={toggleAddExpenseModal}
+			/>
+			<ViewExpenses
+				isOpen={openViewExpenseModal}
+				closeModal={toggleViewExpenseModal}
 			/>
 		</Fragment>
 	)
