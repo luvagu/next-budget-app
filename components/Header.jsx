c7a1bcb224ec4b958f70db08576f84f05a95bd9d
luvagu <luiavag@gmail.com>
2022-01-16 01:34:46 -0500

    addded modal triggers

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,34 @@
+import { Fragment, useState } from 'react'
+import { useBadgets } from '../context/BudgetsContext'
+import AddBudget from './AddBudget'
+import AddExpense from './AddExpense'
 import Button from './shared/Button'
 import Stack from './shared/Stack'
 
 function Header() {
+	const {
+		openAddBudgetModal,
+		toggleBudgetModal,
+		openAddExpenseModal,
+		toggleExpenseModal,
+		openExpenseModalWithId,
+	} = useBadgets()
+
 	return (
-		<Stack direction='horizontal' extraClass='gap-2 mb-4'>
-			<h1 className='text-3xl font-semibold mr-auto'>Budgets</h1>
-			<Button>Add Budget</Button>
-			<Button variant='blue-outline'>Add Expense</Button>
-		</Stack>
+		<Fragment>
+			<Stack direction='horizontal' extraClass='gap-2 mb-4'>
+				<h1 className='text-3xl font-semibold mr-auto'>Budgets</h1>
+				<Button onClick={toggleBudgetModal}>Add Budget</Button>
+				<Button onClick={openExpenseModalWithId} variant='blue-outline'>
+					Add Expense
+				</Button>
+			</Stack>
+			<AddBudget isOpen={openAddBudgetModal} closeModal={toggleBudgetModal} />
+			<AddExpense
+				isOpen={openAddExpenseModal}
+				closeModal={toggleExpenseModal}
+			/>
+		</Fragment>
 	)
 }
 
