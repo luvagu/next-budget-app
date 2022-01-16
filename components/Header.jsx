c36973c931116565cb64e518c5f0406173334de2
luvagu <luiavag@gmail.com>
2022-01-16 18:43:41 -0500

    added user

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,7 @@
-import { Fragment, useState } from 'react'
+import { useUser } from '@auth0/nextjs-auth0'
+import { Fragment } from 'react'
 import { useBadgets } from '../context/BudgetsContext'
+import { LoginIcon } from '@heroicons/react/outline'
 import AddBudget from './AddBudget'
 import AddExpense from './AddExpense'
 import Button from './shared/Button'
@@ -7,6 +9,8 @@ import Stack from './shared/Stack'
 import ViewExpenses from './ViewExpenses'
 
 function Header() {
+	const { user } = useUser()
+
 	const {
 		openAddBudgetModal,
 		toggleAddBudgetModal,
@@ -21,23 +25,35 @@ function Header() {
 		<Fragment>
 			<Stack direction='horizontal' extraClass='gap-2 mb-4'>
 				<h1 className='text-3xl font-semibold mr-auto'>Budgets</h1>
-				<Button onClick={toggleAddBudgetModal}>Add Budget</Button>
-				<Button onClick={openAddExpenseModalWithId} variant='blue-outline'>
-					Add Expense
-				</Button>
+				{user ? (
+					<Fragment>
+						<Button onClick={toggleAddBudgetModal}>Add Budget</Button>
+						<Button onClick={openAddExpenseModalWithId} variant='blue-outline'>
+							Add Expense
+						</Button>
+					</Fragment>
+				) : (
+					<Button onClick={toggleAddBudgetModal} extraClass='flex space-x-2'>
+						<LoginIcon className='h-6 w-6' /> <span>Log In</span>
+					</Button>
+				)}
 			</Stack>
-			<AddBudget
-				isOpen={openAddBudgetModal}
-				closeModal={toggleAddBudgetModal}
-			/>
-			<AddExpense
-				isOpen={openAddExpenseModal}
-				closeModal={toggleAddExpenseModal}
-			/>
-			<ViewExpenses
-				isOpen={openViewExpenseModal}
-				closeModal={toggleViewExpenseModal}
-			/>
+			{user && (
+				<Fragment>
+					<AddBudget
+						isOpen={openAddBudgetModal}
+						closeModal={toggleAddBudgetModal}
+					/>
+					<AddExpense
+						isOpen={openAddExpenseModal}
+						closeModal={toggleAddExpenseModal}
+					/>
+					<ViewExpenses
+						isOpen={openViewExpenseModal}
+						closeModal={toggleViewExpenseModal}
+					/>
+				</Fragment>
+			)}
 		</Fragment>
 	)
 }
