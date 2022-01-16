8ce0edf39bf42ddc7968135548689dbb0e8ad17e
luvagu <luiavag@gmail.com>
2022-01-16 18:43:13 -0500

    set max width

--- components/shared/Container.jsx
+++ components/shared/Container.jsx
@@ -1,5 +1,9 @@
 function Container({ children }) {
-	return <div className='container mx-auto my-4 px-4 md:px-6'>{children}</div>
+	return (
+		<div className='container max-w-7xl mx-auto my-4 px-4 md:px-6'>
+			{children}
+		</div>
+	)
 }
 
 export default Container
