dabc3c71e6abaaed18c4d229de60294dc1f839a2
luvagu <luiavag@gmail.com>
2022-01-16 18:03:43 -0500

    added UserProvider auth0 context component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,15 @@
+import { UserProvider } from '@auth0/nextjs-auth0'
 import BudgetsProvider from '../context/BudgetsContext'
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<BudgetsProvider>
-			<Component {...pageProps} />
-		</BudgetsProvider>
+		<UserProvider>
+			<BudgetsProvider>
+				<Component {...pageProps} />
+			</BudgetsProvider>
+		</UserProvider>
 	)
 }
 
