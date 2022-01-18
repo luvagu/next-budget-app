1fc990d246c455c158a68b1ed2d92da059fdc2f8
luvagu <luiavag@gmail.com>
2022-01-17 23:40:04 -0500

    added fallbackData param

--- hooks/useDbData.js
+++ hooks/useDbData.js
@@ -4,11 +4,12 @@ import useSWR from 'swr'
 const fetcher = url => axios.get(url).then(res => res.data)
 // const fetcher = url => fetch(url).then(r => r.json())
 
-export default function useDbData(endpoint, fallbackData = {}) {
+export default function useDbData(endpoint, fallbackData) {
 	const { data, error, mutate } = useSWR(endpoint, fetcher, { fallbackData })
 
 	return {
 		data,
+		isFetching: !data && !error,
 		isError: error,
 		mutate,
 	}
