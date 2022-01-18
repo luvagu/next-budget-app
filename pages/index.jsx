0b989abbbf8bef270c866bfa11429a47de0cb1c9
luvagu <luiavag@gmail.com>
2022-01-18 00:59:57 -0500

    added metadata and eror flag and Error comp

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,17 +8,33 @@ import UncategorizedBudgetCard from '../components/UncategorizedBudgetCard'
 import TotalBudgetCard from '../components/TotalBudgetCard'
 import Hero from '../components/Hero'
 import Spinner from '../components/shared/Spinner'
+import Error from '../components/shared/Error'
+import Head from 'next/head'
 
 export default function Home() {
 	const { user } = useUser()
-	const { isFetching, budgets, getBudgetExpenses } = useBadgets()
+	const { isFetching, isError, budgets, getBudgetExpenses } = useBadgets()
 
 	return (
 		<Container>
+			<Head>
+				<title>Budgets &amp; Expenses</title>
+				<meta
+					name='description'
+					content='Budgets and Expenses app built with Next.js, Auth0, FaunaDb and Tailwind CSS'
+				/>
+				<meta name='author' content='@luvagu' />
+				<meta charSet='UTF-8' />
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0, shrink-to-fit=no'
+				/>
+			</Head>
 			<Header />
 			{!user && <Hero />}
 			{user && isFetching && <Spinner />}
-			{user && !isFetching && (
+			{user && isError && <Error />}
+			{user && !isFetching && !isError && (
 				<CardsGrid>
 					{budgets?.map(({ id, name, max }) => {
 						const amount = getBudgetExpenses(id).reduce(
