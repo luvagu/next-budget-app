4053c3e8cf69dd05ea578ceafd9d6f12e0a48deb
luvagu <luiavag@gmail.com>
2022-01-22 22:46:12 -0500

    added user and more responsive classes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,15 +1,15 @@
 import HeroContainer from './shared/HeroContainer'
 import { CashIcon } from '@heroicons/react/outline'
 
-function Hero() {
+function Hero({ user }) {
 	return (
 		<HeroContainer>
-			<CashIcon className='h-14 w-14 sm:h-16 sm:w-16 text-blue-600' />
-			<h2 className='text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
-				<span className='block'>Ready to manage budgets?</span>
-				<span className='block text-blue-600'>
-					Please Log In to get started.
-				</span>
+			<CashIcon className='h-12 w-12 sm:h-14 sm:w-14 lg:h-16 lg:w-16 text-blue-600' />
+			<h2 className='text-2xl sm:text-3xl lg:text-4xl font-extrabold tracking-tight text-gray-900 text-center lg:text-left'>
+				<div>Ready to manage budgets?</div>
+				<div className='text-blue-600 text-xl sm:text-2xl'>
+					{user ? 'Visit your dashboard!' : 'Log In to get started.'}
+				</div>
 			</h2>
 		</HeroContainer>
 	)
