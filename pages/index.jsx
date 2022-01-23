fa5b37a5787d0ec09e39b2706d64a2004e81349e
luvagu <luiavag@gmail.com>
2022-01-22 22:49:31 -0500

    moved budgets data away to dashboard page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,60 +1,21 @@
 import { useUser } from '@auth0/nextjs-auth0'
 import Container from '../components/shared/Container'
-import CardsGrid from '../components/CardsGrid'
+import Metatags from '../components/shared/Metatags'
 import Header from '../components/Header'
-import BudgetCard from '../components/BudgetCard'
-import { useBadgets } from '../context/BudgetsContext'
-import UncategorizedBudgetCard from '../components/UncategorizedBudgetCard'
-import TotalBudgetCard from '../components/TotalBudgetCard'
 import Hero from '../components/Hero'
 import Spinner from '../components/shared/Spinner'
 import Error from '../components/shared/Error'
-import Head from 'next/head'
 
 export default function Home() {
-	const { user } = useUser()
-	const { isFetching, isError, budgets, getBudgetExpenses } = useBadgets()
+	const { user, error } = useUser()
 
 	return (
 		<Container>
-			<Head>
-				<title>Budgets &amp; Expenses</title>
-				<meta
-					name='description'
-					content='Budgets and Expenses app built with Next.js, Auth0, FaunaDb and Tailwind CSS'
-				/>
-				<meta name='author' content='@luvagu' />
-				<meta charSet='UTF-8' />
-				<meta
-					name='viewport'
-					content='width=device-width, initial-scale=1.0, shrink-to-fit=no'
-				/>
-			</Head>
-			<Header />
-			{!user && <Hero />}
-			{user && isFetching && <Spinner />}
-			{user && isError && <Error />}
-			{user && !isFetching && !isError && (
-				<CardsGrid>
-					{budgets?.map(({ id, name, max }) => {
-						const amount = getBudgetExpenses(id).reduce(
-							(total, expense) => total + expense.amount,
-							0
-						)
-						return (
-							<BudgetCard
-								key={id}
-								id={id}
-								name={name}
-								amount={amount}
-								max={max}
-							/>
-						)
-					})}
-					<UncategorizedBudgetCard />
-					<TotalBudgetCard />
-				</CardsGrid>
-			)}
+			<Metatags />
+			<Header isHome />
+			{error && <Error />}
+			{user && <Hero user />}
+			{!user && !error && <Hero />}
 		</Container>
 	)
 }
