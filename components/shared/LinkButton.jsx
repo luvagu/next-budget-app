52549d14aef722880e7720a6e5fec6ed00f3f338
luvagu <luiavag@gmail.com>
2022-01-22 22:48:29 -0500

    made it compatible with next/linl when  used as chiled

--- components/shared/LinkButton.jsx
+++ components/shared/LinkButton.jsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react'
 import { classNames } from '../../utils/helpers'
 
 const variants = {
@@ -13,20 +14,26 @@ const sizes = {
 	lg: 'py-2 px-4 sm:py-3 sm:px-6 text-base',
 }
 
-function LinkButton({ children, href, variant = 'blue-outline', size = 'md' }) {
-	return (
-		<a
-			className={classNames(
-				sizes[size],
-				'flex items-center gap-2 border shadow-sm font-semibold whitespace-nowrap rounded-md',
-				variants[variant],
-				'focus:outline-none focus:ring-2 focus:ring-offset-2'
-			)}
-			href={href}
-		>
-			{children}
-		</a>
-	)
-}
+const LinkButton = forwardRef(
+	({ children, variant = 'blue-outline', size = 'md', href, onClick }, ref) => {
+		return (
+			<a
+				className={classNames(
+					sizes[size],
+					'flex items-center gap-2 border shadow-sm font-semibold whitespace-nowrap rounded-md',
+					variants[variant],
+					'focus:outline-none focus:ring-2 focus:ring-offset-2'
+				)}
+				href={href}
+				onClick={onClick}
+				ref={ref}
+			>
+				{children}
+			</a>
+		)
+	}
+)
+
+LinkButton.displayName = 'LinkButton'
 
 export default LinkButton
