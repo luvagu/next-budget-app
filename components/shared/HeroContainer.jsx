2936fa6ab83461450c239e93de9bb4e679788281
luvagu <luiavag@gmail.com>
2022-01-22 22:47:16 -0500

    added error bg color and more responsive classes

--- components/shared/HeroContainer.jsx
+++ components/shared/HeroContainer.jsx
@@ -1,7 +1,14 @@
-function HeroContainer({ children }) {
+import { classNames } from '../../utils/helpers'
+
+function HeroContainer({ children, error }) {
 	return (
-		<div className='bg-gray-50 shadow-sm rounded-md'>
-			<div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 flex flex-col gap-6 items-center justify-center lg:flex-row'>
+		<div
+			className={classNames(
+				error ? 'bg-red-50' : 'bg-gray-50',
+				'shadow-sm rounded-md'
+			)}
+		>
+			<div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 flex flex-col gap-2 sm:gap-4 lg:gap-6 items-center justify-center lg:flex-row'>
 				{children}
 			</div>
 		</div>
