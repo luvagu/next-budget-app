52617645fecf608dff7028fb5d714a4d261ebc69
luvagu <luiavag@gmail.com>
2023-03-30 15:05:22 -0500

    added indication texts

--- components/shared/ProgressBar.jsx
+++ components/shared/ProgressBar.jsx
@@ -19,11 +19,55 @@ const getVariant = ({ percent, isBudgetTypeLoan }) => {
 	return isBudgetTypeLoan ? variants.green : variants.red
 }
 
-function ProgressBar({ current, min = 0, max, isBudgetTypeLoan, isInModal }) {
+function ProgressBar({
+	current,
+	min = 0,
+	max,
+	isBudgetTypeLoan,
+	isInModal,
+	isTotal,
+}) {
 	const percent = calculatePercent(current, max)
+	const isOver = current > max
+	const isAtMax = current === max
+	const remaining = max - current
 
 	return (
 		<>
+			{!isInModal &&
+				(isBudgetTypeLoan ? (
+					<p className='text-xs mb-1'>
+						{isAtMax
+							? 'Congratulation! The loan is paid in full.'
+							: isOver
+							? `You've overpaid the loan by ${curencyFormatter(remaining)}`
+							: `You're ${curencyFormatter(
+									remaining
+							  )} away from repaying the loan.`}
+					</p>
+				) : (
+					<p className='text-xs mb-1'>
+						{isAtMax
+							? isTotal
+								? 'Your total budget is maxed out!'
+								: "You've maxed out the badget."
+							: isOver
+							? isTotal
+								? `Opps! You've gone over the total badget by ${curencyFormatter(
+										remaining
+								  )}`
+								: `Opps! You've gone over the badget by ${curencyFormatter(
+										remaining
+								  )}`
+							: isTotal
+							? `You're ${curencyFormatter(
+									remaining
+							  )} away from maxing out the total budget.`
+							: `You're ${curencyFormatter(
+									remaining
+							  )} away from maxing out the budget.`}
+					</p>
+				))}
 			<div
 				className={classNames(
 					isInModal ? 'relative h-5' : 'h-4',
